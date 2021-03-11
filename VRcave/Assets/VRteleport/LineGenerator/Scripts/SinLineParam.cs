5d3d0ad8b042578d2075104e32f3daf33d3ef052
Toby Huang <huangtoby90@gmail.com>
2021-03-10 21:06:21 -0500

    asset reorganization/ low poly replacement

---
+++ VRcave/Assets/VRteleport/LineGenerator/Scripts/SinLineParam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinLineParam : StraightLineParam, IGenLine{
+	public float xScale = 1;
+	public float yScale = 1;
+	public float offset = 0;
+
+	public float sin(float x){
+		return yScale * Mathf.Sin((x + offset) * xScale);
+	}
+
+	public void genLine(Vector3 start, Vector3 end){
+		line.SetVertexCount(num);
+		//line.SetPosition(0, start);
+
+		float xStep = (end.x - start.x) / num;
+		float zStep = (end.z - start.z) / num;
+		//float yDiff = end.y - start.y;
+
+		for (int i = 0; i < num; i++){
+			//float y = Mathf.Sin(((float)i / num) * (Mathf.PI));
+			float y = sin(i);
+			line.SetPosition(i, new Vector3(start.x + xStep * i, end.y + y, start.z + zStep * i));
+		}
+		//line.SetPosition(num - 1, end);
+	}
+}
