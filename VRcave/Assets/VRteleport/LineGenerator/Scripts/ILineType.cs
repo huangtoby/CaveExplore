332d9b5875990d3f58f6e19aa073c830f105216a
Toby Huang <huangtoby90@gmail.com>
2021-03-28 19:26:33 -0400

    added water movement/moonlight volumetric light

--- VRcave/Assets/VRteleport/LineGenerator/Scripts/ILineType.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-
-public interface IGenLine {
-	void genLine(Vector3 start, Vector3 end);
-}
-
-
---
+++ VRcave/Assets/VRteleporter/LineRender/StraightLineParam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class StraightLineParam : MonoBehaviour, IGenLine{
+
+	public LineRenderer line;
+
+	[Range(0,250)]
+	public int num = 10;
+
+	public virtual void genLine(Vector3 start, Vector3 end){
+		line.SetVertexCount(num);
+
+		for(int i = 0; i < num; i++){
+			Vector3 newPos = Vector3.Lerp(start, end,(float)i/num);// Mathf.Lerp(start.y, end.y,(float)i/num);
+			line.SetPosition(i, newPos);
+		}	
+	}
+}
