a9d48f685f629664edf627e4883ec30b949733a7
Toby Huang <huangtoby90@gmail.com>
2021-04-06 20:25:00 -0400

    object hover highlight

---
+++ VRcave/Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    MeshRenderer emit;
+
+    public void HoverEmit(){
+        emit = GetComponent<MeshRenderer>();
+        emit.material.EnableKeyword("_EMISSION");
+    }
+
+    public void ExitEmit(){
+        emit = GetComponent<MeshRenderer>();
+        emit.material.DisableKeyword("_EMISSION");
+    }
+}
