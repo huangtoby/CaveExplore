f6594e76fe683e95be594a3b945f381f7516bc00
Toby Huang <huangtoby90@gmail.com>
2021-04-20 20:50:11 -0400

    completed portal/ changed force collision

---
+++ VRcave/Assets/Scripts/ForceCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCollider : MonoBehaviour{
+    public GameObject player;
+    public AudioSource collideSound;
+    
+    void OnTriggerEnter(Collider col){
+        collideSound.Play();
+    }
+}
---
+++ VRcave/Assets/Scripts/PortalTP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTP : MonoBehaviour{
+    public GameObject player;
+    public GameObject portalOne;
+    public GameObject portalTwo;
+    private GameObject other;
+    public bool teleported = false;
+    public AudioSource teleportAudio;
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject == player && !teleported){
+            teleportAudio.Play();
+            other = (this.name == portalOne.name) ? portalTwo:portalOne;
+
+            other.GetComponent<PortalTP>().teleported = true;  
+
+            CharacterController cc = player.GetComponent<CharacterController>();
+            cc.enabled = false;
+            player.transform.position = other.transform.position;
+            cc.enabled = true;
+
+            Debug.Log(col.name + " teleports from: "+this.transform.position + " to "+ other.transform.position);  
+        }
+    }
+
+    void OnTriggerExit(Collider col){
+        if(col.gameObject == player){
+            teleported = false;
+        }
+    }
+}
